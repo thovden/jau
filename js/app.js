038e640737c69870bc93f7e2cdf8642acabd068e
Torgeir Hovden <torgeir@comoyo.com>
2014-09-03 13:51:54 +0200

    Working version with Facebook login

--- js/app.js
+++ js/app.js
@@ -8,4 +8,28 @@ app.controller("JauCtrl", ["$scope", "$firebase",
     var sync = $firebase(ref);
     // download the data into a local object
     $scope.data = sync.$asObject();
+
+	$scope.authClient = new FirebaseSimpleLogin(ref, function(error, user) {
+		if (error) {
+		// an error occurred while attempting login
+			console.log(error);
+		} else if (user) {
+		// user authenticated with Firebase
+			$scope.user = user;
+		} else {
+		// user is logged out
+	 		$scope.user = {};
+		}
+	});
+
+var authRef = new Firebase("https://jau.firebaseio.com/.info/authenticated");
+authRef.on("value", function(snap) {
+  if (snap.val() === true) {
+    console.log("authenticated");
+  } else {
+    console.log("not authenticated");
+  }
+});
+
+
 }]);
